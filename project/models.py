f8d62a3640471bc2a4aa321a5bbaac6f89059faf
andreschapa <andres@scanmatics.com>
2021-01-16 14:03:01 -0600

    added unique to company from models:

--- project/models.py
+++ project/models.py
@@ -27,7 +27,7 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    company = db.Column(db.String, nullable=False)
+    company = db.Column(db.String, nullable=False, unique=True)
     customers=db.relationship('Customer', backref='poster')
 
     def __init__(self, name=None, email=None, password=None, company=None):
