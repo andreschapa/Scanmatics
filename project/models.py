2aa493a8380be06cf0f4de62437332f95cc1eeee
andreschapa <andres@scanmatics.com>
2021-01-16 14:26:16 -0600

    added primary key and unique = true to company under users table

--- project/models.py
+++ project/models.py
@@ -24,11 +24,11 @@ class User(db.Model):
 
     __tablename__='users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    company = db.Column(db.String, nullable=False, primary_key=True) ##added primary_key to true to this 1/16/2020
+    company = db.Column(db.String, nullable=False, primary_key=True, unique=True) ##added primary_key to true, and unique=true to this 1/16/2020
     customers=db.relationship('Customer', backref='poster')
     
 
