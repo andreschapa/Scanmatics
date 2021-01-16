ba9172de29e4494c38a6330c41babe8eac320207
andreschapa <andres@scanmatics.com>
2021-01-16 15:41:55 -0600

    test

--- project/models.py
+++ project/models.py
@@ -5,7 +5,7 @@ import datetime
 class User(db.Model):
 
     __tablename__='users'
-    #__table_args__ = (db.UniqueConstraint('company',name='users'),) ##added this line 
+    __table_args__ = (db.UniqueConstraint('company',name='users'),) ##added this line 
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
