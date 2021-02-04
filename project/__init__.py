9b1009c59bd04c51a2dfd31d75699a7467581bef
andreschapa <andres@scanmatics.com>
2021-02-04 17:56:08 -0600

    test

--- project/__init__.py
+++ project/__init__.py
@@ -18,12 +18,25 @@ mail = Mail(app)
 from project.main.views import main_blueprint, send_reset_email
 app.register_blueprint(main_blueprint)
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT']= 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
+#app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+#app.config['MAIL_PORT']= 587
+#app.config['MAIL_USE_TLS'] = True
+#app.config['MAIL_USE_SSL'] = False
+#app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+#app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'main@scanmatics.com',
+    "MAIL_PASSWORD": 'xuchmgcsaejdhxuq'
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
 mail = Mail(app)
 
 
