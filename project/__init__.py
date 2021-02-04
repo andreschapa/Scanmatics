dfc4ca494cffddd04931d086814c36daddea5f34
andreschapa <andres@scanmatics.com>
2021-02-04 17:49:42 -0600

    asdf

--- project/__init__.py
+++ project/__init__.py
@@ -22,8 +22,8 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT']= 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-#app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
-#app.config['MAIL_PASSWORD']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_USERNAME')
 mail = Mail(app)
 
 
