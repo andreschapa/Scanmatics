238c840ee4b86dc5ac442fff3662c85a86dba5bb
andreschapa <andres@scanmatics.com>
2021-03-02 18:21:59 -0600

    asdf

--- project/main/views.py
+++ project/main/views.py
@@ -45,7 +45,8 @@ def MaintenanceLogs(panel_id):
         'QR_dataview_logs.html',
         form=AddMaintenanceLog(request.form),
         open_logs=open_logs,
-        closed_logs=closed_logs
+        closed_logs=closed_logs,
+        panel_id=panel_id
     )
     
 @main_blueprint.route('/NewMaintenanceLog/#<int:panel_id>/', methods=['GET','POST'])
