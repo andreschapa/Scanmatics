2ae3e0413ce50473d5a86e6a0de50f36ecfd8718
andreschapa <andres@scanmatics.com>
2021-03-15 12:37:41 -0500

    asdf

--- project/models.py
+++ project/models.py
@@ -42,7 +42,7 @@ class User(UserMixin, db.Model):
                            #key='myprecious')
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s = Serializer('myprecious', expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
 
