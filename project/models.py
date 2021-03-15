1105562514607ea2801cc6d9258f0e469c69ba11
andreschapa <andres@scanmatics.com>
2021-03-15 13:00:50 -0500

    asdf

--- project/models.py
+++ project/models.py
@@ -37,13 +37,13 @@ class User(UserMixin, db.Model):
 
   
 
-    #def get_reset_token(self, expires=500):
-        #return jwt.encode({'reset_password': self.name, 'exp': time() + expires},
-                           #key='myprecious')
+    def get_reset_token(self, expires=500):
+        return jwt.encode({'reset_password': self.name, 'exp': time() + expires},
+                           key='myprecious')
 
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer('myprecious', expires_sec)
-        return s.dumps({'reset_password': self.name}).decode('utf-8')
+    #def get_reset_token(self, expires_sec=1800):
+        #s = Serializer('myprecious', expires_sec)
+        #return s.dumps({'reset_password': self.name}).decode('utf-8')
 
 
     @staticmethod
